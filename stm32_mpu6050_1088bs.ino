c71d7c327f2aff873fad0af46c5d84ead20d3cdc
Ahmet Utku YILMAZ <ahmetuktuyilmaz01@gmail.com>
2025-06-02 14:49:08 +0300

    Add code description to the code

--- stm32_mpu6050_1088bs.ino
+++ stm32_mpu6050_1088bs.ino
@@ -1,3 +1,15 @@
+/*
+Project: MPU6050 3D Angle Visualization on LED Matrices
+This code obtains raw data from an MPU6050 inertial measurement unit (IMU)
+using the I2C protocol for communication with an STM32 Nucleo-L053R8 board.
+The raw data is processed using the Madgwick sensor fusion algorithm to
+calculate orientation angles (e.g., roll, pitch, yaw).
+These angles are then visualized on two LD1088BS 8x8 LED matrices,
+driven by MAX7219 display driver ICs. Communication with the MAX7219 drivers
+is handled via the SPI protocol from the STM32. The LED matrices aim to
+represent the device's orientation on a conceptual 3D coordinate plane.
+*/
+
 void setup() {
   // put your setup code here, to run once:
 
